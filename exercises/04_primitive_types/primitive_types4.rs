f44135f210e00ce0c6dd85df0c6cb44c348b96ed
brixxt27 <brixxt27@gmail.com>
2025-08-01 02:00:19 +0900

    feat: 배열에서의 slice 문법과 배열의 implicitly coercion(암묵적 강압, 형변환)
    - &[2, 3, 4]는 러스트의 슬라이스 참조라고 한다.
    - assert_eq!에서 첫 번째 인자에서 앰퍼센드를 생략해도 컴파일 에러가 발생하지 않은 이유는 암묵적 형 변환이 일어났기 때문이다.
    - [2, 3, 4]와 &[2, 3, 4]는 타입이 다르다. 전자는 단순 배열이며 후자는 슬라이스 참조이다. 둘은 소유권이 서로 다르다. 배열은 그 자체로 소유권을 갖고 있는데 슬라이스 참조는 소유권을 갖고 있지 않고, 다른 데이터의 일부를 빌려와서 사용하며 런타임에 길이가 결정 됩니다.
    - 소유권이 없다라는 의미는 다음과 같다. 1) 메모리 관리 책임이 없다. 2) 소유권을 갖고 있는 소유자(변수)의 생명주기에 종속 되어, 해당 소유자가 먼저 소멸 되면 댕글링 상태가 되어 유효하지 않은 메모리를 가리키게 되고, 컴파일 에러가 발생한다.
    - 슬라이스 참조는 첫 번째 인자의 주소값과 배열의 길이를 가지고 있다. 이는 Fat Pointer라고도 불린다.

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,8 @@
 fn main() {
     // You can optionally experiment here.
+    let a = [1, 2, 3, 4, 5];
+    println!("a string: {:?}", &a[1..3]);
+    println!("a string: {:?}", [2, 3]); // 첫 번째 요소의 주소와 배열 길이를 저장한다.
 }
 
 #[cfg(test)]
@@ -10,7 +13,10 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+        let nice_slice = &a[1..4];
+        assert_eq!([2, 3, 4], nice_slice);
 
+        let nice_slice = &a[1..=3];
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
