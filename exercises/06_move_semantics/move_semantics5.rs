d2776004db975dbc2b7dece33eff1997a2240c3a
brixxt27 <brixxt27@gmail.com>
2025-08-03 21:15:46 +0900

    헷갈리는 borrowing ownership, moving ownership 개념
    - 해당 문제에서는 &만을 추가하거나 지우면서 컴파일 에러를 해결해야 한다. 여기에서는 먼저 main의 data 변수를 이후에도 사용할 예정인데, get_chat 함수에 소유권을 넘겨버린다. 때문에 이후에 string_uppercase 함수에는 더이상 main이 data 변수의 소유권을 갖고 있지 않기 때문에 참조값을 빌려줄 수 없다.
    - 답안을 보면, get_char에서 &String이 아닌 &str을 사용하는 게 추천된다고 되어있다. String과 str은 서로 다른 타입이다. String은 구조체 그 자체를 가리키고 있는 주소이고, str은 문자열의 첫 주소와 길이의 정보만을 갖고 있다. str은 불변이고, String은 가변이다. 때문에 이 함수에서는 단순히 문자열의  이터레이터를 반환 받아 가장 끝 문자의 이터레이터를 받고, unwrap으로 Option<char> 값의 포장을 해제한다. 이것이 None이라면 강제로 panic 종료를 일으키고, 문자가 존재하면 그대로 진행한다. 해당 문자열은 빈문자열인지 확인하는 함수이다.
    - "" 안에 감싸진 문자열은 문자열 리터럴이므로 static 메모리에 저장되어 이를 수정할 수 없다. 때문에 문자열 내용을 변경하려면 to_string 메서드로 힙에 따로 복사하여 사용한다. 그래서 to_string을 사용하면 안의 내용만 같은 서로 다른 문자열이 된다.

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -4,21 +4,27 @@
 // removing references (the character `&`).
 
 // Shouldn't take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &str) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
+fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
 
     println!("{data}");
 }
 
 fn main() {
-    let data = "Rust is great!".to_string();
+    // let str1 = "";
+    let str1 = "Rust is great!";
+    let data = str1.to_string();
+    // let mut str2 = str1.to_string();
 
-    get_char(data);
+    // str2.push_str(" But C++ is good, too.");
+    // println!("String slice: {str1}\nMutable String is {str2}");
 
-    string_uppercase(&data);
+    get_char(&data);
+
+    string_uppercase(data);
 }
