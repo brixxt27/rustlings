d76bcd48ce9b52196982fb48e4ef8ac26a62f36b
brixxt27 <brixxt27@gmail.com>
2025-08-12 05:48:20 +0900

    09 String 마무리
    - string slice와 owned string의 차이를 학습했습니다. owned string은 heap에 저장되는 변수이고, string slice는 static에 저장되는 변수입니다.
    - 두 타입이 갖고 있는 메서드도 서로 다르기 때문에 필요할 때마다 새로 생성해서 사용해야 합니다.
    - 문자열을 반환하는 메서드도 있지만 원본을 변경하는 메서드도 있으니 원형을 참고해서 사용해야 한다.

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,31 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    
+    // 방법 1) format! 매크로 사용
+    format!("{input} world!")
+    // 방법 2) String으로 변환 후 push_str 사용
+    // let mut result = input.to_owned();
+    // result.push_str(" world!");
+    // result
+    // 방법 3) to_owned() + String
+    // input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", trim_me("\n\n\n\nH e  l l o  W o r l     d!     \na \n"));
+    println!("{}", compose_me("Hello"));
+    println!("{}", replace_me("My cars are very awesome!"));
 }
 
 #[cfg(test)]
