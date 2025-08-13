b8a8e3ad4a25b49842e4b7c634627a896978cb33
brixxt27 <brixxt27@gmail.com>
2025-08-13 21:15:04 +0900

    string slice는 항상 static 메모리 공간에 저장된다는 오해를 풀기 위한 테스트 코드 작성
    - heap에 저장되는 owned string을 string slice를 인자로 받는 함수에 넣어서 정상 작동함을 확인함으로써 기존의 생각이 잘못 되었다는 것을 증명함.

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -9,10 +9,16 @@ fn string(arg: String) {
     println!("{arg}");
 }
 
+fn print_string_slice(arg: &str) {
+    println!("{arg}");
+}
+
+fn main() {
+    print_string_slice(&String::from("value"));
+
 // TODO: Here are a bunch of values - some are `String`, some are `&str`.
 // Your task is to replace `placeholder(…)` with either `string_slice(…)`
 // or `string(…)` depending on what you think each value is.
-fn main() {
     string_slice("blue");
 
     string("red".to_string());
